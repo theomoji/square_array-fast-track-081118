152f62f3a8dd6ea66747a363816c2594c05c7dc2
Shuo Wang <gken90@gmail.com>
2018-08-12 18:19:19 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
 def square_array(array)
-  # your code here
+  new_arr = []
+  array.each do |num|
+    squared = num * num
+    new_arr << squared
+  end
+  return new_arr
 end
+
+# def square_array(array)
+# array.map! { |num| num * num}
+# end
+
+
+# => [1,4,9])
