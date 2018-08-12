581bafa01338513f622d6544674b86ca4f8a4b3c
Shuo Wang <gken90@gmail.com>
2018-08-12 18:14:30 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,14 +1,16 @@
+# def square_array(array)
+#   new_arr = []
+#   array.each do |num|
+#     squared = num * num
+#     new_arr << squared
+#   end
+#   return new_arr
+# end
+
 def square_array(array)
-  new_arr = []
-  array.each do |num|
-    squared = num * num
-    new_arr << squared
-  end
-  return new_arr
+arr.map do |num|
+  num * num
 end
-
-
-
 
 
 # => [1,4,9])
